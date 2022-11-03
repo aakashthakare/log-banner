c9b59d0162c0fe2851019de55b943a20b396aff1
Akash <akash.thakare@thoughtworks.com>
2022-11-03 22:19:38 +0530

    Add argument validation.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@ import in.at.BannerGenerator;
 public class Main {
 
     public static void main(String[] args) {
+        if(args[0] == null || "".equals(args[0].trim())) {
+            System.out.println("Please supply banner input as an argument.");
+        }
         System.out.println(BannerGenerator.generate(args[0]));
     }
 }
